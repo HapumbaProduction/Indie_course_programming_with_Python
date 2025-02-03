c7671ac354decedb7935d50e9cf2cf3b4a8de8cd
HapumbaProduction <kaban050411@gmail.com>
2025-02-03 11:03:04 +0300

    Added the solution to step 23 of lesson 1.5

---
+++ Module_one/Lesson1.5/Lesson1.5_step23.py
@@ -0,0 +1,12 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+
+first_option = first_number + second_number + third_number
+second_option = first_number + second_number * third_number
+third_option = (first_number + second_number) * third_number
+fourth_option = first_number * second_number + third_number
+fifth_option = first_number * (second_number + third_number)
+sixth_option = first_number * second_number * third_number
+
+print(max(first_option, second_option, third_option, fourth_option, fifth_option, sixth_option))
