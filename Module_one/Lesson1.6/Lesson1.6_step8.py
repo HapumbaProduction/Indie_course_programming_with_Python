2410eb6b1b195cb9e7fc45ab821952a7a2e688c1
HapumbaProduction <kaban050411@gmail.com>
2025-02-03 12:49:15 +0300

    Added the solution to step 8 of lesson 1.6

---
+++ Module_one/Lesson1.6/Lesson1.6_step8.py
@@ -0,0 +1,3 @@
+first_assessment, second_assessment, third_assessment, fourth_assessment, fifth_assessment = map(int, input().split())
+
+print(max(first_assessment, second_assessment, third_assessment, fourth_assessment, fifth_assessment))
