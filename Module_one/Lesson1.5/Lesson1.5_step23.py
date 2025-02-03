c357290d7d6dfd3260d285d96080516e643ee629
HapumbaProduction <kaban050411@gmail.com>
2025-02-03 11:25:43 +0300

    Added the solution to step 23 of lesson 1.5

--- Module_one/Lesson1.5/Lesson1.5_step23.py
+++ Module_one/Lesson1.5/Lesson1.5_step23.py
@@ -10,3 +10,4 @@ fifth_option = first_number * (second_number + third_number)
 sixth_option = first_number * second_number * third_number
 
 print(max(first_option, second_option, third_option, fourth_option, fifth_option, sixth_option))
+
