cd2b336d2c27a325f55db02240143711c4627823
HapumbaProduction <kaban050411@gmail.com>
2025-03-03 22:31:31 +0300

    Added task solutions for lecture 2.1

---
+++ Module_two/Lesson2.1/Step23.py
@@ -0,0 +1,5 @@
+first_character, second_character, third_character = map(str, input().split())
+
+print(f"Код символа {first_character} равен {str(ord(first_character))}")
+print(f"Код символа {second_character} равен {str(ord(second_character))}")
+print(f"Код символа {third_character} равен {str(ord(third_character))}")
