142739e8c216664444626917bd117c7e535818ca
HapumbaProduction <kaban050411@gmail.com>
2025-03-17 12:34:05 +0300

    Added task solutions for lecture 2.9

---
+++ Module_two/Lesson2.9/Step11.py
@@ -0,0 +1,1 @@
+print('Для числа {0} предыдущим будет число {1}.'.format(integer_value := int(input()), integer_value - 1), '\nДля числа {0} следующим будет число {1}.'.format(integer_value, integer_value + 1))
---
+++ Module_two/Lesson2.9/Step9.py
@@ -0,0 +1,1 @@
+print('Здравствуйте, {0} {1}!'.format(second_name := input(), first_name := input()))
